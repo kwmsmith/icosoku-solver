f2d6e8e7497493e0cf13af1b4d21c2810152dd02
Kurt Smith <ksmith@enthought.com>
2013-08-25 21:54:16 -0500

    More improvements, cleanup.

--- icosa.py
+++ icosa.py
@@ -20,7 +20,8 @@ pieces = tuple([Piece(i, x) for i, x in
 #   vertex below and to the left of the first vertex listed in the previous
 #   level ('2' in this case).  They are listed in CCW order.
 #   * The vertex opposite '1' is listed last.
-vertices = [1, 2, 5, 10, 8, 6, 11, 12, 4, 3, 7, 9]
+# For instance:
+# vertices = [1, 2, 5, 10, 8, 6, 11, 12, 4, 3, 7, 9]
 
 def verify_vertices(vtxs):
     if vtxs[0] != 1:
@@ -34,78 +35,43 @@ def verify_vertices(vtxs):
 def check_repeats(lol, num):
     return Counter(reduce(operator.add, lol, ())) == {x:num for x in range(1, 13)}
 
-def make_vertex_graph(vtxs):
+def make_faces(vertices):
     '''
-    Takes a list of vertices, returns a dictionary of vertex -> list(vertex)
-    connections.  The list of vertices is in CW order.
-
+    Returns all faces given an ordered list of vertices.  Vertices in clockwise
+    order.  Smallest-valued vertex is first.
     '''
-
-    if not verify_vertices(vtxs):
+    if not verify_vertices(vertices):
         raise ValueError("Vertices not verified!")
 
-    graph = {}
+    first, last = vertices[0], vertices[-1]
+    level1, level2 = vertices[1:6], vertices[6:11]
 
-    first, last = vtxs[0], vtxs[-1]
-    level1, level2 = vtxs[1:6], vtxs[6:11]
+    faces = []
 
-    graph[first] = tuple(reversed(level1))
-    graph[last] = tuple(level2)
+    for idx, v1 in enumerate(level1):
+        faces.append((first, level1[(idx+1)%len(level1)], v1))
+        faces.append((v1, level2[(idx+1)%len(level2)], level2[idx]))
 
-    for idx, vtx in enumerate(level1):
-        next = (idx+1) % len(level1)
-        graph[vtx] = tuple([first, level1[next], level2[next], level2[idx], level1[idx-1]])
+    for idx, v2 in enumerate(level2):
+        faces.append((last, v2, level2[(idx+1)%len(level2)]))
+        faces.append((v2, level1[idx-1], level1[idx]))
 
-    for idx, vtx in enumerate(level2):
-        next = (idx+1) % len(level1)
-        graph[vtx] = tuple([last, level2[idx-1], level1[idx-1], level1[idx], level2[next]])
+    def minrot(f):
+        minf = min(f)
+        if minf == f[0]:
+            return f
+        elif minf == f[1]:
+            return (f[1], f[2], f[0])
+        elif minf == f[2]:
+            return (f[2], f[0], f[1])
+        assert 0
 
-    # Verify that there are 5 unique vertices in the vertex tuples.
-    assert all(len(set(vs)) == 5 for vs in graph.values())
+    faces = sorted(minrot(f) for f in faces)
 
-    # Verify that each vertex shows up exactly 5 times in the vertex tuples.
-    assert check_repeats(graph.values(), 5)
+    assert len(set(faces)) == 20
+    assert check_repeats(faces, 5)
 
-    return graph
-
-def icosa_faces(vtx_graph):
-
-    ifaces = []
-    for vtx, nbrs in sorted(vtx_graph.items()):
-        for idx, nbr in enumerate(nbrs):
-            next = nbrs[(idx+1) % len(nbrs)]
-            if vtx > nbr or vtx > next:
-                continue
-            ifaces.append((vtx, nbr, next))
-
-    # Check that we have 20 unique faces.
-    assert len(set(ifaces)) == 20
-    # Check that each vertex is in exactly 5 faces.
-    assert check_repeats(ifaces, 5)
-
-    return ifaces
-
-def _gen_additive_partitions(p, n, npart, chunk, partitions):
-    if npart < 0:
-        return
-    if sum(p) == n:
-        partitions.append(p)
-        return
-    for i in range(p[-1] if p else chunk, 0, -1):
-        _gen_additive_partitions(p + [i], n, npart-1, chunk, partitions)
-
-def gen_additive_partitions(n, npart, chunk):
-    parts = []
-    _gen_additive_partitions([], n, npart, chunk, parts)
-    return parts
-
-def count_additive_partitions(n, npart, chunk):
-    assert n >= 0
-    if not n:
-        return 1
-    if not npart:
-        return 0
-    return sum(count_additive_partitions(n-i, npart-1, i) for i in range(min(n, chunk), 0, -1))
+    return faces
 
 def rot_piece(piece, r):
     id, val = piece.id, piece.value
@@ -124,8 +90,9 @@ def vertex_to_faces(faces):
     return {v:tuple(f) for v, f in v2f.items()}
 
 def setup_state(pieces, vertices):
-    gr = make_vertex_graph(vertices)
-    faces = icosa_faces(gr)
+
+    faces = make_faces(vertices)
+
     faces_to_pieces = {f:[0,{}] for f in faces}
     for piece in pieces:
         if piece.value[0] == piece.value[1] == piece.value[2]:
@@ -141,10 +108,12 @@ def setup_state(pieces, vertices):
             if rots:
                 faces_to_pieces[face][1][piece] = rots
                 faces_to_pieces[face][0] += len(rots)
+
     pieces_to_face_cnt = {p:0 for p in pieces}
     for face, (cnt, pcs) in faces_to_pieces.items():
         for piece, rots in pcs.items():
             pieces_to_face_cnt[piece] += len(rots)
+
     return faces_to_pieces, pieces_to_face_cnt, vertex_to_faces(faces)
 
 def get_bestface(f2p):
@@ -152,17 +121,18 @@ def get_bestface(f2p):
         return item[1][0]
     return min(f2p.items(), key=keyfunc)[0]
 
-def order_pieces(pieces, p2f):
+def order_pieces(face, pieces, p2f):
     return sorted((p2f[piece], piece, rots) for piece, rots in pieces.items())
 
 def set_bounds(faces_to_pieces, pieces_to_face_cnt, upper_bound, lower_bound, vtx, faces):
+
     for face in faces:
-        # if face == (6,7,8):
-            # import ipdb; ipdb.set_trace()
+        
         try:
             cnt, pieces = faces_to_pieces[face]
         except KeyError:
             continue
+
         idx = face.index(vtx)
         for piece, rots in pieces.items():
             newrots = []
@@ -192,13 +162,13 @@ def search(faces_to_pieces, pieces_to_face_cnt, placements, vtxsum, vtxocc, vtx2
 
     cnt, pieces = faces_to_pieces.pop(bestface)
 
-    # order bestface's pieces by total number of placements overall, including rotations.
-    ordered_pieces = order_pieces(pieces, pieces_to_face_cnt)
-
     # bump the vertex occupancy
     for vtx in bestface:
         vtxocc[vtx] += 1
         assert vtxocc[vtx] <= 5
+
+    # order bestface's pieces by total number of placements overall, including rotations.
+    ordered_pieces = order_pieces(bestface, pieces, pieces_to_face_cnt)
 
     faces_to_pieces_orig = faces_to_pieces
     pieces_to_face_cnt_orig = pieces_to_face_cnt
@@ -229,17 +199,15 @@ def search(faces_to_pieces, pieces_to_face_cnt, placements, vtxsum, vtxocc, vtx2
                 upper_bound = min(vtx - vtxsum[vtx], 3)
                 lower_bound = 0
                 if (5-vtxocc[vtx]) and (vtx - vtxsum[vtx]) > 3 * (5 - vtxocc[vtx]):
-                    cutoff = float(vtx-vtxsum[vtx]) / (5-vtxocc[vtx])
-                    assert cutoff >= 3
-                    lower_bound = cutoff if cutoff >= 3 else 0
+                    lower_bound = float(vtx-vtxsum[vtx]) / (5-vtxocc[vtx])
+                    assert lower_bound >= 3
                 if upper_bound < 0 or lower_bound > 3:
                     break
                 if lower_bound > upper_bound:
                     break
                 set_bounds(faces_to_pieces, pieces_to_face_cnt, upper_bound, lower_bound, vtx, vtx2faces[vtx])
             else:
-                # import ipdb; ipdb.set_trace()
-                placements[bestface] = rpiece
+                placements[bestface] = (rpiece, piece, rot)
                 if search(faces_to_pieces, pieces_to_face_cnt, placements, vtxsum, vtxocc, vtx2faces):
                     return True
                 # reset placements
@@ -257,11 +225,40 @@ def search(faces_to_pieces, pieces_to_face_cnt, placements, vtxsum, vtxocc, vtx2
 
     return False
 
+def verify_solution(pieces, faces, placements):
 
-if __name__ == '__main__':
+    # Check that all faces are included.
+    if set(faces) != set(placements.keys()):
+        return False, "Not all faces are included in placements."
+
+    # make sure we used all pieces in all slots.
+    if set(pieces) != {p for rp, p, r in placements.values()}:
+        return False, "placed pieces not identical to original pieces."
+
+    # Check all vertex sums.
+    for vtx, vfaces in vertex_to_faces(faces).items():
+        vsum = sum(placements[face][0][face.index(vtx)] for face in vfaces)
+        if vsum != vtx:
+            return False, "Vertex %d does not have a valid vertex sum (%d)." % (vtx, vsum)
+
+    return True, "All checks out!"
+
+def main(pieces, vertices):
     f2p, p2f, v2f = setup_state(pieces, vertices)
     placements = {}
     vtxsum = {v:0 for v in vertices}
     vtxocc = vtxsum.copy()
-    # import ipdb; ipdb.set_trace()
     success = search(f2p, p2f, placements, vtxsum, vtxocc, v2f)
+    if success:
+        res, msg = verify_solution(pieces, make_faces(vertices), placements)
+        assert res
+    return {f:rp for f, (rp, p, r) in placements.items()}
+
+
+if __name__ == '__main__':
+    vertices1 = [1, 2, 5, 10, 8, 6, 11, 12, 4, 3, 7, 9]
+    placements = main(pieces, vertices1)
+    vertices2 = range(1, 13)
+    placements2 = main(pieces, vertices2)
+    # vertices3 = [ 1,  8, 12, 11, 10,  9,  7,  6,  5,  4,  3,  2]
+    # placements3 = main(pieces, vertices3)
